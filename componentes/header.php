6fd8067269f814329e35c02c34e4b0cb29727620
David E Luna M <davidlunamontilla@gmail.com>
2019-09-26 15:34:38 -0400

    Se incorpora la imagen en la página Web

--- componentes/header.php
+++ componentes/header.php
@@ -2,14 +2,24 @@
 $header = <<<HTML
 
 <header class="header">
+  
+  
   <!-- $carousel -->
-
   <div class="banner">
     <img class="banner__imagen" src="vista/images/Web-Elementos_0067_Rectángulo-2-copia.png" alt="Home">
   </div>
-
+  
   <div class="default">
     $menuHorizontal
+  </div>
+  
+  <div class="logo">
+
+    <div class="logo__image">
+      <img class="logo__img" src="vista/images/Iniciales_2.png" alt="Logotipo">
+      <p class="logo__p logo__p--small">Save the Date</p>
+      <p class="logo__p logo__p--big">16 de noviembre de 2019</p>
+    </div>
   </div>
 
 </header>
