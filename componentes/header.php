196a661c8b31f0b990af3223726edf5566f27f18
David E Luna M <davidlunamontilla@gmail.com>
2019-09-26 19:03:37 -0400

    Superposición del menú de navegación

--- componentes/header.php
+++ componentes/header.php
@@ -3,6 +3,9 @@ $header = <<<HTML
 
 <header class="header">
   
+  <div class="default navegacion">
+    $menuHorizontal
+  </div>
   
   <!-- $carousel -->
   <div class="banner">
@@ -11,19 +14,16 @@ $header = <<<HTML
   
   <div class="logo--fondo"></div>
   
-  <div class="default">
-    $menuHorizontal
-  </div>
   
   <div class="logo">
-
+    
     <div class="logo__image">
       <img class="logo__img" src="vista/images/Iniciales_2.png" alt="Logotipo">
       <p class="logo__p logo__p--small">Save the Date</p>
       <p class="logo__p logo__p--big">16 de noviembre de 2019</p>
     </div>
   </div>
-
+  
 </header>
 
 HTML;
