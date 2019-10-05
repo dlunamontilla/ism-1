7a6e9c54b44e5f458eaf4a342c2a466258d08766
David E Luna M <davidlunamontilla@gmail.com>
2019-10-05 00:06:14 -0400

    Incorporación del texto con nuevo orden en la página

--- componentes/seccionA.php
+++ componentes/seccionA.php
@@ -9,19 +9,19 @@ $seccionA = <<< HTML
   </div>
 
   <div class="default">
-    <p class="text--image text--center uppercase t--big t--semi-bold">De muchas aventuras juntos, nosotros Fernanda y Gonzalo queremos compartir con ustedes una de las más bonitas, nuestra boda.</p>
+    <p class="text--image text--center uppercase t--big t--semi-bold">De muchas aventuras juntos, nosotros Fernanda y Gonzalo queremos compartir con ustedes una de las más bonitas.</p>
     <!-- <p class="uppercase"><span class="t--big t--semi-bold">Después de vivir maravillosos momentos juntos, queremos oficializar nuestra relación</span></p> -->
     
     <p class="text--center"><img src="vista/images/componentes/noscasamos-v.svg" alt="Nos casamos" class="escala"></p>
-    <p class="text--image text--center uppercase t--big t--semi-bold">Al salir de la iglesia tienes transporte a la fiesta.</p>
-    <!-- <p class="uppercase"><span class="t--big t--semi-bold">Tenemos la inmensa alegría de invitarlos a nuestra boda que se llevará a cabo el día</span></p> -->
+    <p class="uppercase"><span class="t--big t--semi-bold">Tenemos la inmensa alegría de invitarlos a nuestra boda que se llevará a cabo el día</span></p>
+    <!-- <p class="text--image text--center uppercase t--big t--semi-bold">La misa será el día 16 de noviembre, 2019 en la iglesia de San Pedro Apóstol a las 4:00 pm. Al terminar nos encantaría sigas festejando con nosotros.</p> -->
     <p class="text--center"><img src="vista/images/componentes/16112019-v.svg" alt="16 de noviembre de 2019" class="escala"></p>
 
-    <p class="text--image text--center uppercase t--big t--semi-bold">La misa será el día 16 de noviembre, 2019 en la iglesia de San Pedro Apóstol a las 4:00 pm. Al terminar nos encantaría sigas festejando con nosotros.</p>
     <!-- <p class="uppercase m--0"><strong class="t--big">En la parroquia de San Pedro Apóstol</strong></p> -->
-    <p class="t--big uppercase t--semi-bold text--center m-0"><i>¡Esperamos tu asistencia y confirmación!</i></p>
     <!-- <p class="uppercase m--0"><strong class="t--big">A las 04:00 PM</strong></p> -->
 
+    <p class="text--image text--center uppercase t--big t--semi-bold">Al salir de la iglesia tienes transporte a la fiesta.</p>
+    <p class="t--big uppercase t--semi-bold text--center"><i>¡Esperamos tu asistencia y confirmación!</i></p>
     <p class="text--center"><img src="vista/images/componentes/texto-1-v.svg" alt="El amor es el único que crece cuando se reparte" class="escala"></p>
     
     <p class="decoracion t--center">
