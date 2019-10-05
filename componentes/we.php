ec6816ba9e44cbdc80154adab918372d2e2b931f
David E Luna M <davidlunamontilla@gmail.com>
2019-10-04 23:42:03 -0400

    Cambio de sección del texto

--- componentes/we.php
+++ componentes/we.php
@@ -7,14 +7,6 @@ $we = <<< HTML
     <!-- <span>You <span class="t-color--secundario">+</span> Me <span class="t-color--secundario">=</span> We</span> -->
     <img src="vista/images/componentes/youmewe-v.svg" class="text--image centrado--h" alt="You + Me = We">
   </div>
-
-  <div class="default">
-    <p class="text--image">De muchas aventuras juntos, nosotros Fernanda y Gonzalo queremos compartir con ustedes una de las más bonitas, nuestra boda.</p>
-    <p class="text--image">La misa será el día 16 de noviembre, 2019 en la iglesia de San Pedro Apóstol a las 4:00 pm. Al terminar nos encantaría sigas festejando con nosotros.</p>
-    <p class="text--image">Al salir de la iglesia tienes transporte a la fiesta.</p>
-
-    <p class="t--2 text--center m-0"><i>¡Esperamos tu asistencia y confirmación!</i></p>
-  </div>
 </div>
 
 
