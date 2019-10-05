4be5c9b823e8fb8cb8bbb3176f8bdbaf0b21bd5a
David E Luna M <davidlunamontilla@gmail.com>
2019-10-05 01:07:04 -0400

    Ajuste del mapa de Google

--- componentes/como-llegar.php
+++ componentes/como-llegar.php
@@ -40,7 +40,7 @@ $comoLlegar = <<<HTML
     </div>
 
     <div class="mapas">
-    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.114667682329!2d-89.57380378506737!3d21.028097385998723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f56771479e0a39f%3A0x1152caa81b57b1ce!2sCity%20Express%20Junior%20M%C3%A9rida%20Altabrisa!5e0!3m2!1ses-419!2sve!4v1570249143782!5m2!1ses-419!2sve" width="600" height="450" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+      <iframe class="mapas__iframe" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3724.114667682329!2d-89.57380378506737!3d21.028097385998723!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x8f56771479e0a39f%3A0x1152caa81b57b1ce!2sCity%20Express%20Junior%20M%C3%A9rida%20Altabrisa!5e0!3m2!1ses-419!2sve!4v1570249143782!5m2!1ses-419!2sve" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
     </div>
   </div>
 
