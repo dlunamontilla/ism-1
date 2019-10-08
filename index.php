e43a262a388fd880f93eb174728cc3d03195c435
David E Luna M <davidlunamontilla@gmail.com>
2019-10-08 14:32:18 -0400

    Actualización de la versión de la hoja de estilos

--- index.php
+++ index.php
@@ -22,7 +22,7 @@
     
     <!-- Estilos principales -->
     <link rel="stylesheet" href="vista/css/fonts.css?v=1.1">
-    <link rel="stylesheet" href="vista/css/style.css?v=1.20">
+    <link rel="stylesheet" href="vista/css/style.css?v=1.21">
 
     <!-- Favicon -->
     <link rel="icon" sizes="any" href="favicon.png" type="image/png">
