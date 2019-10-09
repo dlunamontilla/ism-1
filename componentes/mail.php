44575188dbd1e64053c87b77200aa220b46da617
David E Luna M <davidlunamontilla@gmail.com>
2019-10-09 19:40:25 -0400

    Formato HTML - Parte 2

--- componentes/mail.php
+++ componentes/mail.php
@@ -16,8 +16,13 @@ $cabeceras .= 'Content-type: text/html; charset=UTF-8' . "\r\n";
 $cabeceras .= 'To: David E Luna M <davidlunamontilla@outlook.com>' . "\r\n";
 $cabeceras .= $from;
 
+$box ='box-sizing: border-box;';
+$styleBody = 'margin: 0; padding: 0; font-size: 1rem; color: #606060; background-color: #f0f0f0; font-family: "Open Sans", sans-serif, arial;' . $box;
+$styleMail = 'margin: 20px auto; border: 1px solid white; background-color: #f9f9f9; box-shadow: 0 0 2px silver; max-width: 1200px; width: calc(100% - 20px); padding: 30px; line-height: 1.6;' . $box;
+
 
 $html = <<<HTML
+
 <!DOCTYPE html>
 <html lang="es-ES">
 <head>
@@ -25,37 +30,10 @@ $html = <<<HTML
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
   <title>Invitación</title>
-
-  <style>
-    body, html {
-      margin: 0;
-      padding: 0;
-      font-size: 1rem;
-      color: #606060;
-      background-color: #f0f0f0;
-      font-family: "Open Sans", sans-serif, arial;
-    }
-
-    *, *::before, *::after {
-      box-sizing: border-box;
-    }
-
-    .mail {
-      margin: 20px auto;
-      border: 1px solid white;
-      background-color: #f9f9f9;
-      box-shadow: 0 0 2px silver;
-      max-width: 1200px;
-      width: calc(100% - 20px);
-      padding: 30px;
-      line-height: 1.6;
-    }
-
-  </style>
 </head>
 
-<body>
-  <main class="mail">
+<body style="$styleBody">
+  <main style="$styleMail">
     <h1>Un título HTML</h1>
     <hr>
     <p>Contenido del párrafo</p>
