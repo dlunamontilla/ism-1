aa00fc1bcbe97915318a6571becd2a1f913ba2c1
David E Luna M <davidlunamontilla@gmail.com>
2019-10-09 20:40:10 -0400

    Formato HTML - Última prueba

--- componentes/mail.php
+++ componentes/mail.php
@@ -2,7 +2,6 @@
 // fernanda.schnierle@gmail.com
 // boda2019@gonzaloyfernanda.com
 
-// mail ( string $to , string $subject , string $message [, string $additional_headers [, string $additional_parameters ]] ) : bool
 $from = 'From: Invitación <boda2019@gonzaloyfernanda.com>' . "\r\n";
 
 $para = 'davidlunamontilla@outlook.com';
@@ -17,9 +16,20 @@ $cabeceras .= 'To: David E Luna M <davidlunamontilla@outlook.com>' . "\r\n";
 $cabeceras .= $from;
 
 $box ='box-sizing: border-box;';
-$styleBody = 'margin: 0; padding: 0; font-size: 1rem; color: #606060; background-color: white; font-family: \'Open Sans\', sans-serif, arial;' . $box;
+$styleBody = 'margin: 0; padding: 0; font-size: 1rem; color: #295053; background-color: white; font-family: \'Open Sans\', sans-serif, arial;' . $box;
 
 $hr = '<hr style="border: 0; border-bottom: 1px solid #3400be;">';
+
+// Procesamiento de imágenes:
+$rutaImagen = '../vista/images/Iniciales_2.png';
+$imagen = "";
+
+if ( file_exists($rutaImagen) ) {
+  $imdata = file_get_contents($rutaImagen);
+  $imdata = base64_encode($imdata);
+  $attr = 'alt="Logotipo" style="float: left; width: 160px; max-width: calc(100 - 20px);"';
+  $imagen = "<img src='data:image/png;base64," . $imdata . "' " . $attr . " />";
+}
 
 $html = <<<HTML
 
@@ -37,16 +47,17 @@ $html = <<<HTML
       <div style="margin: 0 auto; max-width: 800px; padding: 30px; background-color: #f9f9f9">
           
         <div style="display: flex; justify-content: flex-start; align-items: center;">
-          <img src="../vista/images/Iniciales_2.svg" alt="Logotipo" style="float: left; width: 150px; max-width: calc(100 - 20px);">
-          <h1 style="font-variant: small-caps; color: #e69c00; font-size: 2rem; margin: 0;">Invitación</h1>
+          <!-- <img src="../vista/images/Iniciales_2.svg" alt="Logotipo" style="float: left; width: 150px; max-width: calc(100 - 20px);"> -->
+          $imagen
+          <h1 style="padding-left: 30px; font-variant: small-caps; color: #489da4; font-size: 2rem; margin: 0;">Invitación</h1>
         </div>
 
         $hr
 
         <h3 style="font-size: 1.5rem; font-variant: small-caps; margin: 0 auto; margin-top: 30px;">Datos del Invitado</h3>
-        <ul style="padding: 0 30px; margin: 0">
-          <li><span><strong>Nombres: </strong><br>David E Luna M</span></li>
-          <li style="margin-top: 10px"><span><strong>Participantes: </strong><br>10 personas</span></li>
+        <ul style="padding: 0 30px; margin: 0;">
+          <li style="font-variant: small-caps"><span><strong>Nombres: </strong><br>David E Luna M</span></li>
+          <li style="margin-top: 10px; font-variant: small-caps"><span><strong>Participantes: </strong><br>10 personas</span></li>
         </ul>
     </div>
 
